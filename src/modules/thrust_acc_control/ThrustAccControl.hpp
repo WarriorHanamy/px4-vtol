3b5f2256c14a2e15503996ff2a46de6ff394fc45
WarriorHanamy <632029496@qq.com>
2024-05-19 07:42:17 +0800

    Change the acc to slowly down

--- src/modules/thrust_acc_control/ThrustAccControl.hpp
+++ src/modules/thrust_acc_control/ThrustAccControl.hpp
@@ -33,109 +33,112 @@
 
 #pragma once
 
-#include <lib/rate_control/rate_control.hpp>
+#include <lib/adv_control_lib/butterworth_filter.h>
 #include <lib/matrix/matrix/math.hpp>
 #include <lib/perf/perf_counter.h>
+#include <lib/rate_control/rate_control.hpp>
+#include <lib/systemlib/mavlink_log.h>
 #include <px4_platform_common/defines.h>
 #include <px4_platform_common/module.h>
 #include <px4_platform_common/module_params.h>
 #include <px4_platform_common/posix.h>
 #include <px4_platform_common/px4_work_queue/ScheduledWorkItem.hpp>
-#include <lib/systemlib/mavlink_log.h>
 #include <uORB/Publication.hpp>
 #include <uORB/PublicationMulti.hpp>
 #include <uORB/Subscription.hpp>
 #include <uORB/SubscriptionCallback.hpp>
 #include <uORB/topics/parameter_update.h>
 #include <uORB/topics/rate_ctrl_status.h>
-#include <uORB/topics/vehicle_angular_velocity.h>
+#include <uORB/topics/sensor_gyro.h>
 #include <uORB/topics/vehicle_acceleration.h>
+#include <uORB/topics/vehicle_angular_velocity.h>
+#include <uORB/topics/vehicle_attitude.h>
 #include <uORB/topics/vehicle_control_mode.h>
 #include <uORB/topics/vehicle_rates_setpoint.h>
 #include <uORB/topics/vehicle_status.h>
-#include <uORB/topics/vehicle_attitude.h>
-#include <uORB/topics/sensor_gyro.h>
-#include <uORB/topics/vehicle_angular_velocity.h>
-#include <lib/adv_control_lib/butterworth_filter.h>
 // #include <uORB/topics/vehicle_
 // #include <uORB/topics/vehithrus
 #include <uORB/topics/vehicle_thrust_acc_setpoint.h>
 // #include <uORB/topics/vehilce_thrust_acc_setpoint.h>
 using namespace time_literals;
 
-class ThrustAccControl : public ModuleBase<ThrustAccControl>, public ModuleParams, public px4::WorkItem
-{
+class ThrustAccControl : public ModuleBase<ThrustAccControl>,
+                         public ModuleParams,
+                         public px4::WorkItem {
 public:
-	ThrustAccControl();
-	~ThrustAccControl() override;
+  ThrustAccControl();
+  ~ThrustAccControl() override;
 
-	/** @see ModuleBase */
-	static int task_spawn(int argc, char *argv[]);
+  /** @see ModuleBase */
+  static int task_spawn(int argc, char *argv[]);
 
-	/** @see ModuleBase */
-	static int custom_command(int argc, char *argv[]);
+  /** @see ModuleBase */
+  static int custom_command(int argc, char *argv[]);
 
-	/** @see ModuleBase */
-	static int print_usage(const char *reason = nullptr);
+  /** @see ModuleBase */
+  static int print_usage(const char *reason = nullptr);
 
-	bool init();
+  bool init();
 
 private:
-	void Run() override;
-	void resetButterworthFilter();
-	/**
-	 * initialize some vectors/matrices from parameters
-	 */
-	void parameters_updated();
+  void Run() override;
+  void resetButterworthFilter();
+  /**
+   * initialize some vectors/matrices from parameters
+   */
+  void parameters_updated();
 
-	float get_u_inverse_model(float target_at);
+  float get_u_inverse_model(float target_at);
 
+  uORB::SubscriptionData<vehicle_thrust_acc_setpoint_s>
+      _vehicle_thrust_acc_setpoint_sub{ORB_ID(vehicle_thrust_acc_setpoint)};
+  uORB::Subscription _vehicle_status_sub{ORB_ID(vehicle_status)};
+  uORB::Subscription _vehicle_control_mode_sub{ORB_ID(vehicle_control_mode)};
+  uORB::Subscription _vehicle_attitude_sub{ORB_ID(vehicle_attitude)};
+  uORB::SubscriptionData<sensor_gyro_s> _acc_b_sub{ORB_ID(sensor_gyro)};
+  uORB::SubscriptionData<vehicle_acceleration_s> _vacc_sub{
+      ORB_ID(vehicle_acceleration)};
 
+  uORB::SubscriptionInterval _parameter_update_sub{ORB_ID(parameter_update),
+                                                   1_s};
 
-	uORB::SubscriptionData<vehicle_thrust_acc_setpoint_s> _vehicle_thrust_acc_setpoint_sub{ORB_ID(vehicle_thrust_acc_setpoint)};
-	uORB::Subscription _vehicle_status_sub{ORB_ID(vehicle_status)};
-	uORB::Subscription _vehicle_control_mode_sub{ORB_ID(vehicle_control_mode)};
-	uORB::Subscription _vehicle_attitude_sub{ORB_ID(vehicle_attitude)};
-	uORB::SubscriptionData<sensor_gyro_s> _acc_b_sub{ORB_ID(sensor_gyro)};
-	uORB::SubscriptionData<vehicle_acceleration_s> _vacc_sub{ORB_ID(vehicle_acceleration)};
+  uORB::SubscriptionCallbackWorkItem _vehicle_angular_velocity_sub{
+      this, ORB_ID(vehicle_angular_velocity)};
 
-	uORB::SubscriptionInterval _parameter_update_sub{ORB_ID(parameter_update), 1_s};
+  uORB::Publication<vehicle_rates_setpoint_s> _vehicle_rates_setpoint_pub{
+      ORB_ID(vehicle_rates_setpoint)};
 
-	uORB::SubscriptionCallbackWorkItem _vehicle_angular_velocity_sub{this, ORB_ID(vehicle_angular_velocity)};
+  orb_advert_t _mavlink_log_pub{nullptr}; ///< mavlink log pub
 
-	uORB::Publication<vehicle_rates_setpoint_s>	_vehicle_rates_setpoint_pub{ORB_ID(vehicle_rates_setpoint)};
+  RateControl _rate_control;
 
-	orb_advert_t		_mavlink_log_pub{nullptr};	///< mavlink log pub
+  vehicle_control_mode_s _vehicle_control_mode{};
 
-	RateControl _rate_control;
+  float thrust_sp;
 
-	vehicle_control_mode_s	_vehicle_control_mode{};
+  hrt_abstime _last_run{0};
 
-	float thrust_sp;
+  perf_counter_t _loop_perf; /**< loop duration performance counter */
 
+  // keep setpoint values between updates
+  matrix::Vector3f _rates_setpoint{};
+  vehicle_thrust_acc_setpoint_s _thrust_acc_setpoint_msg;
+  float _thrust_acc_sp{};
+  matrix::Quaternionf _rotate_q{};
+  float _thr_p;
 
-	hrt_abstime _last_run{0};
+  // we assumes the model of thrust is quadratic, i.e. a_t = a*u
+  float _thr_lin_k;
 
-	perf_counter_t	_loop_perf;			/**< loop duration performance counter */
+  float _timeout_acc = 9.6;
+  uint64_t _timeout_time = 0;
+  ButterworthFilter2nd _lpf;
 
-	// keep setpoint values between updates
-	matrix::Vector3f _rates_setpoint{};
-	vehicle_thrust_acc_setpoint_s _thrust_acc_setpoint_msg;
-	float _thrust_acc_sp{};
-	matrix::Quaternionf _rotate_q{};
-	float _thr_p;
-
-	// we assumes the model of thrust is quadratic, i.e. a_t = a*u
-	float _thr_lin_k;
-
-	float _timeout_acc = 9.9;
-	uint64_t _timeout_time = 0;
-	ButterworthFilter2nd _lpf;
-
-	DEFINE_PARAMETERS(
-		(ParamFloat<px4::params::THR_P>) _param_thr_p,
-		(ParamFloat<px4::params::THR_CUR_LIN_K>) _param_thr_lin_k,
-		(ParamFloat<px4::params::THR_TMO_ACC>) _param_thr_timeout_acc,
-		(ParamInt<px4::params::THR_TMO_TIME>) _param_sys_timeout_time  /**< example parameter */
-	)
+  DEFINE_PARAMETERS((ParamFloat<px4::params::THR_P>)_param_thr_p,
+                    (ParamFloat<px4::params::THR_CUR_LIN_K>)_param_thr_lin_k,
+                    (ParamFloat<px4::params::THR_TMO_ACC>)
+                        _param_thr_timeout_acc,
+                    (ParamInt<px4::params::THR_TMO_TIME>)
+                        _param_sys_timeout_time /**< example parameter */
+  )
 };
